0b2f4f4bf3c3e10490fe7b78864b82bbcf232e30
Nadir Uriel Rosas Millan <144179674+NadirUrielRosasMillan@users.noreply.github.com>
2023-10-15 13:56:08 -0600

    Add files via upload

---
+++ p1./p2_1s21.java
@@ -0,0 +1,13 @@
+public class p2_1s21 {
+    public static void main(String[] args) {
+       
+       
+        //ln me permite mi salto de pagina//
+        System.out.println("Nombre: Nadir ");
+        System.out.println("Edad: 18 años");
+        System.out.println("Correo electronico: nadirrosas2905@gmail.com");
+        System.out.println("Aficionado: Box");
+        System.out.println("Color Favrito: Azul");
+
+    }
+}
